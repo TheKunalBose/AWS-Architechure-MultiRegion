b20693df2eae1fb923002ac4fbaa79fe7cc3a87d
kunal <kunalvon3002@gmail.com>
2024-12-12 20:51:30 +0530

    Changes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "kbzbucket"
+    bucket         = "kbzstatebucket"
     key            = "aws-cloud/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
